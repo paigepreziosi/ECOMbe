e628860de9569cbdec02307cc34f0710b47d031c
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-11 01:35:06 -0400

    commit models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      referneces: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
+    produt_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
